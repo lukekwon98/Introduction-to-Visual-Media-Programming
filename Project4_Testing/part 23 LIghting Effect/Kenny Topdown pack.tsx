3e6c12dada825d30894398f3c7e8fd9b15116d24
lukekwon98 <48666394+lukekwon98@users.noreply.github.com>
2023-01-10 17:10:18 +0900

    Delete Project4_Testing/part 23 LIghting Effect directory

--- Project4_Testing/part 23 LIghting Effect/Kenny Topdown pack.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Kenny Topdown pack" tilewidth="64" tileheight="64" spacing="10" tilecount="540" columns="27">
- <image source="img/spritesheet_tiles.png" width="1988" height="1470"/>
-</tileset>
